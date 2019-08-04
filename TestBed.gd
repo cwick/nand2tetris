28a5a80c6d7b9021fd373807c06c40f65f3db689
Carmen Wick <CarmenWick@gmail.com>
2019-08-04 13:15:05 -0700

    OR

--- TestBed.gd
+++ TestBed.gd
@@ -1,19 +1,10 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	run()
 	
 func run():
 	$Output.text = String($NAND.apply($InputA.pressed, $InputB.pressed))
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func _on_input_pressed():
 	run()
---
+++ chips/or/OR.gd
@@ -0,0 +1,14 @@
+extends Object
+
+const AND = preload("res://chips/and/AND.gd")
+const NOT = preload("res://chips/not/NOT.gd")
+var _and = AND.new()
+var _not = NOT.new()
+
+func apply(a: bool, b: bool) -> bool:
+	return _not.apply(_and.apply(_not.apply(a), _not.apply(b)))
+	
+func _notification(what):
+	if what == NOTIFICATION_PREDELETE:
+		_and.free()
+		_not.free()
