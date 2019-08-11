db6e1a3abe40f7edf30b6ba44e0f553a1c0a9ba9
Carmen Wick <CarmenWick@gmail.com>
2019-08-11 01:10:28 -0700

    Start tree of chips

--- tests/chip.gd
+++ tests/chip.gd
@@ -1,6 +1,5 @@
 extends Reference
 
-var input_pin_count = 0
 var _parts = {}
 var _connections = {}
 var _output_part_name = null
@@ -9,14 +8,12 @@ func evaluate(input):
 	if _parts.size() == 0:
 		return false
 
-	var output_part = _parts[_output_part_name]
-	var part_input = []
+	var output_node = ChipNode.new(_parts[_output_part_name])
+	for part_input_pin in _connections:
+		var chip_input_pin = _connections[part_input_pin]
+		output_node.add_child_at(InputNode.new(input, chip_input_pin), part_input_pin)
 
-	part_input.resize(output_part.input_pin_count)
-	for i in range(output_part.input_pin_count):
-		part_input[i] = input[_connections[i]]
-
-	return output_part.evaluate(part_input)
+	return output_node.evaluate()
 
 func connect_part(part_name, part_input_pin, chip_input_pin):
 	_connections[part_input_pin] = chip_input_pin
@@ -26,3 +23,35 @@ func add_part(part_name, part):
 
 func connect_output(part_name):
 	_output_part_name = part_name
+
+
+class ChipNode:
+	var _child_nodes = []
+	var _chip
+
+	func _init(chip):
+		_chip = chip
+		
+	func evaluate():
+		var input_values = []
+		for child in _child_nodes:
+			input_values.append(child.evaluate())
+		return _chip.evaluate(input_values)
+
+	func add_child_at(child, i):
+		if i >= _child_nodes.size() - 1:
+			_child_nodes.resize(i+1)
+
+		_child_nodes[i] = child
+
+
+class InputNode:
+	var _input
+	var _selector
+
+	func _init(input, selector):
+		_input = input
+		_selector = selector
+
+	func evaluate():
+		return _input[_selector]
--- tests/test_chip.gd
+++ tests/test_chip.gd
@@ -22,13 +22,12 @@ func test_empty_chip():
 func test_true_chip():
 	chip.add_part("true", TrueChip.new())
 	chip.connect_output("true")
-	
+
 	assert_true(chip.evaluate([]))
 
 func test_nand():
 	chip.add_part("nand", NandChip.new())
 	chip.connect_output("nand")
-	chip.input_pin_count = 2
 	chip.connect_part("nand", 0, 0)
 	chip.connect_part("nand", 1, 1)
 	chip.connect_output("nand")
@@ -42,26 +41,34 @@ func test_nand():
 		""")
 
 func test_not():
-	chip.add_part("not", NandChip.new())
-	chip.connect_output("not")
-	chip.input_pin_count = 1
-	chip.connect_part("nand", 0, 0)
-	chip.connect_part("nand", 1, 0)
-
-	assert_truth_table(chip, 
+	assert_truth_table(_make_not_chip(), 
 		"""
 		1 0
 		0 1
 		""")
 
+# func test_not_not():
+# 	chip.add_part("not1", _make_not_chip())
+# 	chip.add_part("not2", _make_not_chip())
+# 	chip.connect_output("not2")
+# 	chip.connect_part("not1", 0, 0)
+# 	chip.connect_part("not2", 0, "not1")
+
+# 	assert_truth_table(_make_not_chip(), 
+# 		"""
+# 		1 1
+# 		0 0
+# 		""")
+
 # func test_and():
-# 	chip.add_part("", NandChip.new())
-# 	chip.add_part("", NandChip.new())
-# 	chip.input_pin_count = 2
+# 	chip.add_part("nand1", NandChip.new())
+# 	chip.add_part("nand2", NandChip.new())
+# 	chip.connect_output("nand2")
 # 	chip.connect_part("nand1", 0, 0)
 # 	chip.connect_part("nand1", 1, 1)
 # 	chip.connect_part("nand2", 0, "nand1", 0)
 # 	chip.connect_part("nand2", 1, "nand1", 0)
+
 # 	assert_truth_table(chip, 
 # 		"""
 # 		1 1 1
@@ -69,6 +76,14 @@ func test_not():
 # 		0 1 0
 # 		0 0 0
 # 		""")
+
+func _make_not_chip():
+	var chip = Chip.new()
+	chip.add_part("not", NandChip.new())
+	chip.connect_output("not")
+	chip.connect_part("nand", 0, 0)
+	chip.connect_part("nand", 1, 0)
+	return chip
 
 func assert_truth_table(chip, truth_table):
 	var no_allow_empty = false
